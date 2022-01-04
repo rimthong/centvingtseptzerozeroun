31379a6acc204fe0d5c55cc75aba5ad17f960924
Alexandre Rimthong <rimthong@gmail.com>
2022-01-03 23:41:32 -0500

    Fixing typo

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Cent Vingt-Sept Zero Zero Un`,
+    description: `Projet de techno franco`,
     author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    siteUrl: `https://centvingtseptzerozeroun.com/`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
